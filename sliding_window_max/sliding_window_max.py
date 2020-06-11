bea5c5250e4b00d028e20c10427c11555f496dd6
AceIsHuman <menoman1995@yahoo.com>
2020-06-11 16:10:51 -0500

    solve sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -11,10 +11,29 @@ Returns: a List of integers
 # return list of all stored largest values
 
 def sliding_window_max(nums, k):
-    # Your code here
+    # create window start and end indices
+    start, end = 0, k - 1
+    # create list to store max values
+    max_vals = []
+    # while end is less than length of nums
+    while end < len(nums):
+    ## compare values in window and add largest to max values
+        max_num = compare_window(nums, start, end)
+        max_vals.append(max_num)
+    ## once compare is complete increase window indices
+        start += 1
+        end += 1
+    # return max values
+    return max_vals
 
-    pass
-
+def compare_window(nums, start, end):
+    pointer = start + 1
+    max_num = nums[start]
+    while pointer <= end:
+        if max_num < nums[pointer]:
+            max_num = nums[pointer]
+        pointer += 1
+    return max_num
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
