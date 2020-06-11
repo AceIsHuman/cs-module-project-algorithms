079c15c5f7d273cdeec05da2eabe038a4361c849
AceIsHuman <menoman1995@yahoo.com>
2020-06-11 15:33:34 -0500

    take notes to understand problem

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,6 +2,14 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+# Understand
+
+# We will look at `k` values from left to right and store the largest value
+# `k` determines the size of our viewing window
+# the end of the window must not pass the last index of list
+# after each pass move window start and end positions to right by one index
+# return list of all stored largest values
+
 def sliding_window_max(nums, k):
     # Your code here
 
